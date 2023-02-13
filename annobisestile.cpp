d197281cf1934e49adf4c639d5110220981c7b92
ma018ma <marzia018@libero.it>
2023-02-13 14:54:30 +0100

    corretto esercizio anna bisestile

--- annobisestile.cpp
+++ annobisestile.cpp
@@ -8,11 +8,11 @@ int main()
     std::cout << "inserisci un anno:";
     std::cin >> numero;
 
-    if (numero % 400 == 0)
+    if (numero % 400 == 0 && numero % 100 == 1)
     {
         std::cout << "questo anno e bisestile";
     }
-    else if (numero % 4 == 0 && numero % 100 == 1)
+    else if (numero % 4 == 0)
         {
             std::cout << "questo anno e bisestile";
         }
