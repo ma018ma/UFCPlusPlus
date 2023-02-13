177a5b1de4d2fe1a351d896f1ee62c7d16f65da4
ma018ma <marzia018@libero.it>
2023-02-13 14:24:56 +0100

    corretto esercizio somma di 100n

--- sommadi100n.cpp
+++ sommadi100n.cpp
@@ -4,12 +4,11 @@ using namespace std;
 
 int main()
 {
-    int n = 0;
     int i = 0;
     int somma = 0;
 
     for (i=0;i<=100;i++){
-        somma += n+i;
-     cout<< somma;
-    }    
+        somma += i;
+    } 
+    cout<< somma;   
 }
