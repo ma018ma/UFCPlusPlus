a6b7be6b1106706577210a5354d4c7402746adae
ma018ma <marzia018@libero.it>
2023-02-20 09:33:30 +0100

    corretta lapiugrande

--- stampalapiugrande.cpp
+++ stampalapiugrande.cpp
@@ -3,20 +3,21 @@
 using namespace std;
 
 string parolamaggiore(string parola)
-{   
-    string parolalunga = 0;
-    for (int i=0;i<=10;i++)
+{
+    string parolalunga; 
+    std::cout<<"dammi 10 parole"<<endl;
+    for (int i=0;i<10;i++)
     {
-        if (parola>parolalunga){
-        parola = parolalunga;
+        cin>>parola;
+        if (parola.length()>parolalunga.length())
+        {
+        parolalunga=parola;
         } 
-        else 
     }
-        std::cout<< parolamaggiore("")
+    cout<<"questa e' la parola piu' lunga:"<< parolalunga<<"."<<endl;
 }
-   
 
 int main()
 {
-    
+    std::cout<<parolamaggiore("");
 }
